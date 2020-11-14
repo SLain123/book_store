028d9a86e12ab01eea0d1ead6a4ee95fada99f73
SLain <sl163@ya.ru>
2020-11-14 13:05:30 +0300

    Создает сборщик проекта; Файловая структура проекта; Модуль спиннер;

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Spinner from './components/spinner';
+
+const App = () => {
+    return (
+        <div>
+            <p>App</p>
+            <Spinner />
+        </div>
+    )
+}
+
+export default App;
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div class='loader'>
+            <div class='spinner yellow'></div>
+            <div class='spinner orange'></div>
+            <div class='spinner red'></div>
+            <div class='spinner pink'></div>
+            <div class='spinner violet'></div>
+            <div class='spinner mauve'></div>
+            <div class='spinner light-yellow'></div>
+        </div>
+    )
+}
+
+export default Spinner;
