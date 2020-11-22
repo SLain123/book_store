c659f2760ca098e1d284018459f62af6bb4bf367
SLain <sl163@ya.ru>
2020-11-22 18:01:05 +0300

    Разделяет компонент book list на рендер компонент и  логику: bookListContainer;

--- src/components/book_list/book_list.js
+++ src/components/book_list/book_list.js
@@ -8,7 +8,66 @@ import ErrorIndicator from '../error_indicator';
 
 import './book_list.scss';
 
-class BookList extends Component {
+const BookList = ({ books }) => {
+    return (
+        <section>
+            <ul className='book-list row'>
+                {books.map(
+                    ({
+                        title,
+                        author,
+                        year,
+                        id,
+                        price,
+                        imgUrlJPG,
+                        imgUrlWEBP,
+                    }) => {
+                        return (
+                            <li
+                                key={id}
+                                className='book-list__item col-xl-6 col-12'
+                            >
+                                <picture>
+                                    <source
+                                        srcSet={imgUrlWEBP}
+                                        type='image/webp'
+                                    ></source>
+                                    <img
+                                        className='book-list__img'
+                                        src={imgUrlJPG}
+                                        alt='Обложка'
+                                        width='220'
+                                        height='310'
+                                    />
+                                </picture>
+                                <div className='book-list__info-container'>
+                                    <p className='book-list__title'>{title}</p>
+                                    <p className='book-list__describe'>
+                                        Автор: {author}
+                                    </p>
+                                    <p className='book-list__describe'>
+                                        Год издания: {year}
+                                    </p>
+                                    <p className='book-list__price'>
+                                        {price} руб.
+                                    </p>
+                                    <button
+                                        type='button'
+                                        className='book-list__add-btn btn btn-info'
+                                    >
+                                        Добавить в корзину
+                                    </button>
+                                </div>
+                            </li>
+                        );
+                    },
+                )}
+            </ul>
+        </section>
+    );
+};
+
+class BookListContainer extends Component {
     componentDidMount() {
         this.props.downloadBooks();
     }
@@ -24,64 +83,7 @@ class BookList extends Component {
             return <ErrorIndicator />;
         }
 
-        return (
-            <section>
-                <ul className='book-list row'>
-                    {books.map(
-                        ({
-                            title,
-                            author,
-                            year,
-                            id,
-                            price,
-                            imgUrlJPG,
-                            imgUrlWEBP,
-                        }) => {
-                            return (
-                                <li
-                                    key={id}
-                                    className='book-list__item col-xl-6 col-12'
-                                >
-                                    <picture>
-                                        <source
-                                            srcSet={imgUrlWEBP}
-                                            type='image/webp'
-                                        ></source>
-                                        <img
-                                            className='book-list__img'
-                                            src={imgUrlJPG}
-                                            alt='Обложка'
-                                            width='220'
-                                            height='310'
-                                        />
-                                    </picture>
-                                    <div className='book-list__info-container'>
-                                        <p className='book-list__title'>
-                                            {title}
-                                        </p>
-                                        <p className='book-list__describe'>
-                                            Автор: {author}
-                                        </p>
-                                        <p className='book-list__describe'>
-                                            Год издания: {year}
-                                        </p>
-                                        <p className='book-list__price'>
-                                            {price} руб.
-                                        </p>
-                                        <button
-                                            type='button'
-                                            className='book-list__add-btn btn btn-info'
-                                        >
-                                            Добавить в корзину
-                                        </button>
-                                    </div>
-                                </li>
-                            );
-                        },
-                    )}
-                </ul>
-            </section>
-        );
+        return <BookList books={books} />;
     }
 }
 
@@ -100,5 +102,5 @@ const mapDispatchToProps = (dispatch, ownProps) => {
 };
 
 export default withBookstoreServiceConsumer(
-    connect(mapStateToProps, mapDispatchToProps)(BookList),
+    connect(mapStateToProps, mapDispatchToProps)(BookListContainer),
 );
