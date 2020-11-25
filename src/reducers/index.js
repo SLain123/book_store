259c9e06153e7ebabe8bca950a5ba7e391f00a13
SLain <sl163@ya.ru>
2020-11-25 09:19:11 +0300

    Сокращает и оптимизирует код функций в reducer;

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,77 +6,46 @@ const initialState = {
     total: 500,
 };
 
-//---------------------------------------------------------------------------
-//Функци для формирования item;
-//--------------------------------------------------------------------------
-
-// Под-функция для изменения содержимого уже добавленного ранее в корзину item; 
-// Может прибавлять или отнимать значение количества через переданный аргумент opt: plus & minus;
-
-const changeExistItem = (state, actionId, index, opt) => {
+const createEditRemoveItemToState = (state, actionId, count) => {
     const selectBook = state.books.find(({ id }) => id === actionId);
+    const index = state.cartItems.findIndex(({ id }) => id === actionId);
     const item = {
         id: selectBook.id,
         titleBook: selectBook.title,
     };
-    if (opt === 'plus') {
-        item.countBook = state.cartItems[index].countBook + 1;
-        item.totalPrice = state.cartItems[index].totalPrice + selectBook.price;
+
+    if (index !== -1) {
+        item.countBook = state.cartItems[index].countBook + count;
+        item.totalPrice =
+            state.cartItems[index].totalPrice + count * selectBook.price;
+
+        if (item.countBook < 1) {
+            return {
+                ...state,
+                cartItems: [
+                    ...state.cartItems.slice(0, index),
+                    ...state.cartItems.slice(index + 1),
+                ],
+            };
+        }
+
+        return {
+            ...state,
+            cartItems: [
+                ...state.cartItems.slice(0, index),
+                item,
+                ...state.cartItems.slice(index + 1),
+            ],
+        };
     } else {
-        item.countBook = state.cartItems[index].countBook - 1;
-        item.totalPrice = state.cartItems[index].totalPrice - selectBook.price;
+        item.countBook = 1;
+        item.totalPrice = selectBook.price;
+
+        return {
+            ...state,
+            cartItems: [...state.cartItems, item],
+        };
     }
-    return item;
-};
-
-// Под-функция для создания нового item которого еще нет в корзине;
-
-const createNewItem = (state, actionId) => {
-    const selectBook = state.books.find(({ id }) => id === actionId);
-    return {
-        id: selectBook.id,
-        titleBook: selectBook.title,
-        countBook: 1,
-        totalPrice: selectBook.price,
-    };
-};
-
-// -----------------------------------------------------------------
-// Функции для работы непосредственно в направлении state: add, change, remove;
-//------------------------------------------------------------------
-
-// Функция добавляет новый item ---> state;
-
-const addItemToState = (state, actionId) => {
-    return {
-        ...state,
-        cartItems: [...state.cartItems, createNewItem(state, actionId)],
-    };
-};
-
-// Функция добавляет измененный item на зарезервированное им место в массиве cartItems в state;
-
-const changeItemInState = (state, index, actionId, opt) => {
-    return {
-        ...state,
-        cartItems: [
-            ...state.cartItems.slice(0, index),
-            changeExistItem(state, actionId, index, opt),
-            ...state.cartItems.slice(index + 1),
-        ],
-    };
-};
-
-// Функция удаляет нужный item from state;
-
-const removeItemToState = (state, index) => {
-    return {
-        ...state,
-        cartItems: [
-            ...state.cartItems.slice(0, index),
-            ...state.cartItems.slice(index + 1),
-        ],
-    };
 };
 
 //---------------------------------------------
@@ -110,40 +79,24 @@ const reducer = (state = initialState, action) => {
             };
         }
         case 'ADD_ITEM': {
-            const index = state.cartItems.findIndex(
-                ({ id }) => id === action.id,
-            );
-
-            if (index !== -1) {
-                return changeItemInState(state, index, action.id, 'plus');
-            } else {
-                return addItemToState(state, action.id);
-            }
+            return createEditRemoveItemToState(state, action.id, 1);
         }
         case 'DECREASE_ITEM_COUNT': {
-            const index = state.cartItems.findIndex(
-                ({ id }) => id === action.id,
-            );
-
-            if (state.cartItems[index].countBook === 1) {
-                return removeItemToState(state, index);
-            } else {
-                return changeItemInState(state, index, action.id, 'minus');
-            }
+            return createEditRemoveItemToState(state, action.id, -1);
         }
         case 'INCREASE_ITEM_COUNT': {
-            const index = state.cartItems.findIndex(
-                ({ id }) => id === action.id,
-            );
-
-            return changeItemInState(state, index, action.id, 'plus');
+            return createEditRemoveItemToState(state, action.id, 1);
         }
         case 'REMOVE_ITEM': {
-            const index = state.cartItems.findIndex(
+            const itemInCart = state.cartItems.find(
                 ({ id }) => id === action.id,
             );
 
-            return removeItemToState(state, index);
+            return createEditRemoveItemToState(
+                state,
+                action.id,
+                -itemInCart.countBook,
+            );
         }
         default:
             return state;
